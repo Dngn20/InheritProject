dc852ad083a3b7cee288e42f21eadf0c9a903238
Dngn20 <dngn20@gmail.com>
2017-04-12 12:42:30 -0600

    Did both frame and the panel

--- src/inherit/view/InheritFrame.java
+++ src/inherit/view/InheritFrame.java
@@ -1,5 +1,28 @@
 package inherit.view;
 
-public class InheritFrame {
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import inherit.controller.*;
 
+public class InheritFrame extends JFrame
+{
+	private InheritController baseController;
+	private InheritPanel appPanel;
+	
+	public InheritFrame(InheritController baseControlller)
+	{
+		super();
+		this.baseController = baseController;
+		appPanel = new InheritPanel(baseController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPanel);
+		this.setTitle("ChatBot");
+		this.setSize(new Dimension (800, 600));
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 }
--- src/inherit/view/InheritPanel.java
+++ src/inherit/view/InheritPanel.java
@@ -7,6 +7,8 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
 import inherit.controller.InheritController;
@@ -15,7 +17,9 @@ public class InheritPanel extends JPanel
 {
 	private InheritController baseController;
 	private SpringLayout baseLayout;
-	
+	private JButton button1;
+	private JButton button2;
+	private JTextArea textArea;
 	
 	
 	
@@ -24,6 +28,11 @@ public class InheritPanel extends JPanel
 		super();
 		this.baseController = baseController;
 		this.baseLayout = new SpringLayout();
+		this.button1 = new JButton("Button 1");
+		this.button2 = new JButton("Button 2");
+		this.textArea = new JTextArea(25,60);
+		
+		
 		
 		
 		
@@ -36,13 +45,24 @@ public class InheritPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.setBackground(Color.darkGray);
+		this.setPreferredSize(new Dimension(900, 600));
+		this.add(button1);
+		this.add(button2);
+		this.add(textArea);
 	}
 
 
 	public void setupLayout()
 	{
-		
+		baseLayout.putConstraint(SpringLayout.WEST, button1, 121, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, button2, 0, SpringLayout.NORTH, button1);
+		baseLayout.putConstraint(SpringLayout.EAST, button2, -199, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, button1, 49, SpringLayout.SOUTH, textArea);
+		baseLayout.putConstraint(SpringLayout.NORTH, textArea, 36, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, textArea, 85, SpringLayout.WEST, this);
+
 	}
 
 	public void setupListener()
